9f47568554f822e86d14099d06a126bd99533e56
Bojan Stepic <bstepic@hotmail.com>
2017-09-20 20:19:40 +0200

    final fix
    test comment

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-(function() {
-  // get necessary elements
-  var allItems = document.querySelector(".all-items").children;
-  var experienceItems = document.querySelector(".professional-list");
+(function () {
+    // get necessary elements
+    var allItems = document.querySelector(".all-items").children;
+    var experienceItems = document.querySelector(".professional-list");
 
-  // experience items click handler
-  experienceItems.onclick = function(e) {
-    var currentSection = e.target.getAttribute("id");
-    if (currentSection) {
-      var len = allItems.length;
-      var currentItem;
-      while (len) {
-        len--;
-        currentItem = allItems[len];
-        currentItem.classList.add("hidden");
-        if (currentItem.className.startsWith(currentSection)) {
-          currentItem.classList.remove("hidden");
+    // experience items click handler
+    experienceItems.onclick = function (e) {
+        var currentSection = e.target.getAttribute("id");
+        if (currentSection) {
+            var len = allItems.length;
+            var currentItem;
+            while (len) {
+                len--;
+                currentItem = allItems[len];
+                currentItem.classList.add("hidden");
+                if (currentItem.className.startsWith(currentSection)) {
+                    currentItem.classList.remove("hidden");
+                }
+            }
         }
-      }
-    }
-  };
+    };
 })();
